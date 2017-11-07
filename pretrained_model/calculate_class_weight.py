c505c522de9d9311bde20a2ad98b63f5eff22265
Yuki Tsuji <yukitsuji020832@gmail.com>
2017-11-08 00:37:26 +0900

    Complete calculating class weight

--- pretrained_model/calculate_class_weight.py
+++ pretrained_model/calculate_class_weight.py
@@ -8,14 +8,24 @@ import os
 
 def parse_arg():
     parser = argparse.ArgumentParser()
+    parser.add_argument("method", help="mean or log")
     parser.add_argument("--base_dir", type=str, default="./")
-    parser.add_argument("--result", type=str, default="class_weight.npy")
-    parser.add_argument('--source', type=str, required=True, help='absolute path to your data file')
-    parser.add_argument('--num_classes', type=int, required=True, help='absolute path to your data file')
+    parser.add_argument("--result", type=str, default="class_weight")
+    parser.add_argument('--source', type=str, required=True,
+                        help='absolute path to your data file')
+    parser.add_argument('--num_classes', type=int, required=True,
+                        help='absolute path to your data file')
     return parser.parse_args()
 
-def calc_median_frequency():
+def calc_median_frequency(classes):
+    class_freq = np.array(classes)
+    median_freq = np.mean(class_freq)
+    return median_freq / class_freq
 
+def calc_log_frequency(classes, value=1.02):
+    class_freq = np.array(classes)
+    class_freq /= class_freq.sum()
+    return 1 / np.log(value + class_freq)
 
 if __name__ == '__main__':
     args = parse_arg()
@@ -32,14 +42,13 @@ if __name__ == '__main__':
             for nc in range(args.num_classes):
                 classes[nc] += (labels == nc).sum()
 
-        if 0 in classes:
-            raise Exception("Some classes are not found")
+    if 0 in classes:
+        raise Exception("Some classes are not found")
 
-        class_freq = np.array(classes)
-        median_freq = np.mean(class_freq)
+    if args.method == "mean"
+        class_weight = calc_median_frequency(classes)
+    elif args.method == "log":
+        class_weight = calc_log_frequency(classes)
 
-        for c in range(args.num_classes):
-            a[c] = float(median_freq) / float(freq[c])
-            print('    class_weighting: {:.4f}'.format(a[m]))
-
-        print("Done!")
+    np.save(args.result, class_weight)
+    print("Done!")
